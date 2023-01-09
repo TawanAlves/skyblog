76672c30ed141e14f4666816429c2723a80452f6
TawanAlves <tas.tawan@.com>
2023-01-08 22:45:50 -0300

    iniciada pagina para criação de posts

--- client/src/pages/CreatePost/index.tsx
+++ client/src/pages/CreatePost/index.tsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 //css
-import { Container } from "./styles";
+import { Container, Form, ImageInput, TextInput, TitleInput } from "./styles";
 
 const CreatePost: React.FC = () => {
+  const [title, setTitle] = useState("");
+
   return (
     <Container>
       <h1>Criar Post</h1>
+      <Form>
+        <TitleInput>
+          Titulo da Postagem:
+          <TextInput
+            required
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
+        </TitleInput>
+        <ImageInput></ImageInput>
+      </Form>
     </Container>
   );
 };
--- client/src/pages/CreatePost/styles.tsx
+++ client/src/pages/CreatePost/styles.tsx
@@ -4,4 +4,32 @@ export const Container = styled.div``;
 
 export const Form = styled.form``;
 
-export const TextInput = styled.input``;
+export const TitleInput = styled.label`
+  display: flex;
+  flex-direction: column;
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+export const TextInput = styled.input.attrs({ type: "text" })`
+  margin-top: 4px;
+  color: grey;
+  outline: none;
+  width: 300px;
+  height: 20px;
+`;
+
+export const ImageInput = styled.label`
+  display: flex;
+  flex-direction: column;
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+// export const TextInput = styled.input.attrs({ type: "text" })`
+//   margin-top: 4px;
+//   color: grey;
+//   outline: none;
+//   width: 300px;
+//   height: 20px;
+// `;
