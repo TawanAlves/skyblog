86e98f8cd4d40c11ba2c4eb63f3d13fb0aaca559
TawanAlves <tas.tawan@.com>
2023-01-08 23:24:47 -0300

    criado formulario de submição da postagem

--- client/src/pages/CreatePost/index.tsx
+++ client/src/pages/CreatePost/index.tsx
@@ -1,15 +1,42 @@
 import React, { useState } from "react";
 
 //css
-import { Container, Form, ImageInput, TextInput, TitleInput } from "./styles";
+import {
+  Button,
+  Container,
+  EmailInput,
+  Form,
+  ImageInput,
+  NameInput,
+  TextInput,
+  TitleInput,
+} from "./styles";
 
 const CreatePost: React.FC = () => {
   const [title, setTitle] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
   return (
     <Container>
       <h1>Criar Post</h1>
       <Form>
+        <TitleInput>
+          Nome:
+          <NameInput
+            required
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+        </TitleInput>
+        <TitleInput>
+          Email:
+          <EmailInput
+            required
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </TitleInput>
         <TitleInput>
           Titulo da Postagem:
           <TextInput
@@ -18,7 +45,11 @@ const CreatePost: React.FC = () => {
             value={title}
           />
         </TitleInput>
-        <ImageInput></ImageInput>
+        <ImageInput>
+          Imagem:
+          <input type="file" name="arquivos" />
+        </ImageInput>
+        <Button />
       </Form>
     </Container>
   );
--- client/src/pages/CreatePost/styles.tsx
+++ client/src/pages/CreatePost/styles.tsx
@@ -2,34 +2,57 @@ import styled from "styled-components";
 
 export const Container = styled.div``;
 
-export const Form = styled.form``;
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 export const TitleInput = styled.label`
+  margin-top: 10px;
   display: flex;
   flex-direction: column;
   font-size: 20px;
   font-weight: 600;
 `;
 
+export const NameInput = styled.input.attrs({ type: "text" })`
+  margin-top: 10px;
+  color: black;
+  outline: none;
+  width: 300px;
+  height: 20px;
+`;
+
+export const EmailInput = styled.input.attrs({ type: "email" })`
+  color: black;
+  outline: none;
+  width: 300px;
+  height: 20px;
+`;
+
 export const TextInput = styled.input.attrs({ type: "text" })`
-  margin-top: 4px;
-  color: grey;
+  margin-top: 10px;
+  color: black;
   outline: none;
   width: 300px;
   height: 20px;
 `;
 
 export const ImageInput = styled.label`
-  display: flex;
-  flex-direction: column;
+  margin-top: 15px;
   font-size: 20px;
   font-weight: 600;
+  display: flex;
+  flex-direction: column;
 `;
 
-// export const TextInput = styled.input.attrs({ type: "text" })`
-//   margin-top: 4px;
-//   color: grey;
-//   outline: none;
-//   width: 300px;
-//   height: 20px;
-// `;
+export const Button = styled.input.attrs({ type: "submit" })`
+  margin-top: 15px;
+  padding: 20px;
+  color: white;
+  background-color: #000000;
+  font-weight: 600;
+  border: none;
+  border-radius: 40px;
+`;
